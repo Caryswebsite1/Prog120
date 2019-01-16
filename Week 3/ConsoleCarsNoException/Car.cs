5d62f48468eb0da7aafa114743055b337c2bccd8
Caryswebsite1 <46387153+Caryswebsite1@users.noreply.github.com>
2019-01-16 13:59:58 -0800

    Add files via upload

---
+++ Week 3/ConsoleCarsNoException/Car.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Car
+    {
+        // fields
+        private decimal _CurrentSpeedMPH; // the cars current speed
+        private decimal _MaxSpeedMPH;  // the upper limit, the car cannot exceed this value
+        private bool _EngineRunning;  // true means the engine has been started and is running, false says engine is off
+
+        public Car(decimal maxSpeed) // constuctor, user chooses to set maxSpeed as they create the object
+        {
+            _MaxSpeedMPH = maxSpeed;
+            _CurrentSpeedMPH = 0;
+            _EngineRunning = false;
+        }
+
+      
+        public bool EngineRunning   // Property to allow reading the current value
+        {   get   {   return _EngineRunning;   }   }
+
+        public decimal CurrentSpeedMPH    // Property to allow reading the current value
+        {   get   {   return _CurrentSpeedMPH;   }   }
+     
+
+    
+        public void StartEngine()  // method used to start car's engine
+        {
+           if(_EngineRunning)
+            {
+                throw new ApplicationException("Starter Complains Loudly!  The engine is already running!");
+            }
+            _EngineRunning = true;
+        }
+
+        public void StopEngine()    // method used to stop car's engine
+        {
+            if (!_EngineRunning)
+            {
+                throw new ApplicationException("Nothing happens.... The engine is already off!");
+            }
+            _EngineRunning = false;
+        }
+
+
+        public void Accelerate()  // method used to increase the cars speed
+        {
+            if (!_EngineRunning)
+            {
+                throw new ApplicationException("You are disapointed by the absolute lack of accelleration. The engine is Off!");
+            }
+            else
+            {
+                _CurrentSpeedMPH = _CurrentSpeedMPH + 10;  // this method always tries to increase by 10 mph
+                if (_CurrentSpeedMPH > _MaxSpeedMPH)  // but it can never exceed the max speed as set in the constructor
+                {
+                    _CurrentSpeedMPH = _MaxSpeedMPH;  // if speed is higher than max, slow it to max
+                }
+            }
+        }
+
+        public void Decelerate()  // this method always tries to decrease the speed by 5 mph
+        {
+            _CurrentSpeedMPH = _CurrentSpeedMPH - 5;  // but never lets it go below 0 and become negative.
+            if (_CurrentSpeedMPH < 0)
+            {
+                _CurrentSpeedMPH = 0;
+            }
+        }
+
+       
+    }
+}
---
+++ Week 3/ConsoleCarsNoException/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleCarsExcpeptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car(35);  // call constructor and set speed max to 35 (tiny engine!!)
+            string ErrorMessage = "All is well";
+
+
+            //PUT  try catches in while loop switch to catch throws from car class and to set ErrorMessage
+            bool bHadError = false;  // to check if we had an error this loop
+            bool done = false; // loop control variable
+            while (!done)
+            {
+                bHadError = false;  // not really needed here...
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("start = start engine, stop = stop engine");
+                Console.Write("accel = accelerate, brake = brake, q = quit. Command? :");
+                Console.ResetColor();
+                string userInput = Console.ReadLine();
+                userInput = userInput.ToLower();  // in case user happened to enter capital letters
+                Console.Clear();  // each time we get a new command, clear the prior history from the console
+
+                try
+                {
+
+                    switch (userInput)
+                    {
+                        case "start":
+                            myCar.StartEngine();
+                            break;
+                        case "stop":
+                            myCar.StopEngine();
+                            break;
+                        case "accel":
+                            myCar.Accelerate();
+                            break;
+                        case "brake":
+                            myCar.Decelerate();
+                            break;
+                        case "q":
+                        case "e":
+                            Console.WriteLine("Goodbye");
+                            done = true;
+                            break;
+                        default:
+                            Console.WriteLine("Not a valid input.");
+                            break;
+                    }
+                }// end try
+                catch(ApplicationException ex)
+                {
+                    ErrorMessage = ex.Message;
+                    bHadError = true;
+                    // if one of ours, set the ErrorMessage to exceptions message,
+                    // if not, then set ErrorMessage to "All is well" 
+                }
+
+               
+                // at the end of each command, call this method to show status
+                DisplayCarState(ErrorMessage, myCar);
+                if (bHadError)  // reset error message now
+                {
+                    ErrorMessage = "All is well";
+                    bHadError = false;
+                }
+            }
+            Console.ReadLine();
+
+        }
+
+        private static void DisplayCarState(string msg, Car myCar)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(msg);
+            Console.WriteLine("Engine is running: " + myCar.EngineRunning);
+            Console.WriteLine("Current speed is: " + myCar.CurrentSpeedMPH);
+            Console.ResetColor();
+        }
+    }
+}
